8c82aafef6280ee6c3c92951e75c65d455c8e4a5
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-07-19 23:17:39 -0700

    create testing dataset

---
+++ data/test.py
@@ -0,0 +1,37 @@
+from re import sub
+from os import mkdir
+from csv import writer
+from json import loads
+from requests import get
+from os.path import exists
+
+if exists("data/temp/dictionary.jsonl"):
+    with open("data/temp/dictionary.jsonl") as file:
+        dictionary = file.read().split("\n")
+else:
+    res = get("https://kaikki.org/dictionary/Korean/kaikki.org-dictionary-Korean.jsonl").text
+    dictionary = res.split("\n")
+    if not exists("data/temp"):
+        mkdir("data/temp")
+    with open("data/temp/dictionary.jsonl", "w") as file:
+        file.write(res)
+
+words = []
+for word in dictionary:
+    if not word:
+        continue
+    word = loads(word)
+    if not "forms" in word:
+        continue
+    for form in word["forms"]:
+        if not "roman" in form or not "form" in form:
+            continue
+        romaja = sub(r"[^a-z\s]+", "", form["roman"])
+        korean = sub(r"[^가-힣\s]", "", form["form"])
+        words.append((romaja, korean))
+
+print(f"generated {len(words)} pairs")
+with open("data/test.csv", "w") as file:
+    csv = writer(file)
+    csv.writerow(["romaja", "korean"])
+    csv.writerows(words)
