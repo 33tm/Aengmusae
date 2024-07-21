758f20a3fcf4d866e7c055a4d98cca2828af7208
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-07-20 21:29:48 -0700

    better logging

--- data/train.py
+++ data/train.py
@@ -22,59 +22,73 @@ def getElapsed():
     return str(elapsed).split('.')[0]
 
 def getSongs(page):
-    url = f"https://colorcodedlyrics.com/category/krn/page/{page}"
-    soup = BeautifulSoup(get(url).content, "html.parser")
-    songs = {a["href"] for a in soup.find_all("a", rel="bookmark")}
-    return list(songs)
+    try:
+        url = f"https://colorcodedlyrics.com/category/krn/page/{page}"
+        soup = BeautifulSoup(get(url).content, "html.parser")
+        songs = {a["href"] for a in soup.find_all("a", rel="bookmark")}
+        print(f"{getElapsed()} - page {page}", end="\r")
+        return list(songs)
+    except Exception as e:
+        if e.__class__.__name__ == "ConnectionError":
+            return getSongs(page)
+        print(f"{e} - {url}")
+        return []
 
 def getLyrics(url):
-    soup = BeautifulSoup(get(url).content, "html.parser")
-    head = [
-        item.getText() for item in
-        soup.select("table[border='0'] th")
-        or
-        soup.select(".wp-block-column > p > strong > span")
-        or
-        soup.select("table")[1].select("th")
-    ]
-    body = [
-        item.getText() for item in
-        soup.select("table[border='0'] td")
-        or
-        soup.select(".wp-block-column > .wp-block-group > div")
-        or
-        soup.select("table")[1].select("td")
-    ]
+    try:
+        soup = BeautifulSoup(get(url).content, "html.parser")
+        head = [
+            item.getText() for item in
+            soup.select("table[border='0'] th")
+            or
+            soup.select(".wp-block-column > p > strong > span")
+            or
+            soup.select("table")[1].select("th")
+        ]
+        body = [
+            item.getText() for item in
+            soup.select("table[border='0'] td")
+            or
+            soup.select(".wp-block-column > .wp-block-group > div")
+            or
+            soup.select("table")[1].select("td")
+        ]
 
-    if not "Romanization" in head or "Japanese" in head:
+        if not "Romanization" in head or "Japanese" in head:
+            return []
+
+        romaja = body[head.index("Romanization")]
+        korean = body[head.index("Korean") if "Korean" in head else head.index("Hangul")]
+
+        romaja = [word for word in romaja.lower().split("\n") if word]
+        korean = [word for word in korean.lower().split("\n") if word]
+
+        if len(romaja) != len(korean):
+            return []
+        
+        lyrics = []
+        for r, k in zip(romaja, korean):
+            r, k = trimDuplicate(r.strip(), k.strip())
+            if not r or not k:
+                continue
+            r, k = r.split(" "), k.split(" ")
+            if len(r) != len(k):
+                continue
+            r_line, k_line = [], []
+            for i in range(len(r)):
+                r[i], k[i] = trimDuplicate(r[i], k[i])
+                r[i] = r[i].replace("-", "")
+                if match(r"^[a-z\s]+$", r[i]) and match(r"^[가-힣\s]+$", k[i]):
+                    r_line.append(r[i])
+                    k_line.append(k[i])
+            lyrics.append((" ".join(r_line), " ".join(k_line)))
+        print(f"{getElapsed()} - {url.split('/')[-2].replace('-', ' ')}", end="\r")
+        return lyrics
+    except Exception as e:
+        if e.__class__.__name__ == "ConnectionError":
+            return getLyrics(url)
+        print(f"{e} - {url}")
         return []
-
-    romaja = body[head.index("Romanization")]
-    korean = body[head.index("Korean") if "Korean" in head else head.index("Hangul")]
-
-    romaja = [word for word in romaja.lower().split("\n") if word]
-    korean = [word for word in korean.lower().split("\n") if word]
-
-    if len(romaja) != len(korean):
-        return []
-    
-    lyrics = []
-    for r, k in zip(romaja, korean):
-        r, k = trimDuplicate(r.strip(), k.strip())
-        if not r or not k:
-            continue
-        r, k = r.split(" "), k.split(" ")
-        if len(r) != len(k):
-            continue
-        r_line, k_line = [], []
-        for i in range(len(r)):
-            r[i], k[i] = trimDuplicate(r[i], k[i])
-            r[i] = r[i].replace("-", "")
-            if match(r"^[a-z\s]+$", r[i]) and match(r"^[가-힣\s]+$", k[i]):
-                r_line.append(r[i])
-                k_line.append(k[i])
-        lyrics.append((" ".join(r_line), " ".join(k_line)))
-    return lyrics
 
 if exists("temp/songs.txt"):
     with open("temp/songs.txt", "r") as file:
@@ -87,11 +101,7 @@ else:
         for i in range(1465):
             futures.append(executor.submit(getSongs, i))
     for i, future in enumerate(as_completed(futures)):
-        try:
-            songs += future.result()
-            print(f"page {i + 1}/1465 - {getElapsed()}")
-        except Exception as e:
-            print(f"page {i + 1}/1465 - {e}")
+        songs += future.result()
     if not exists("temp"):
         mkdir("temp")
     with open("temp/songs.txt", "w") as file:
@@ -103,10 +113,7 @@ with ThreadPoolExecutor(max_workers=10) as executor:
     for song in songs:
         futures.append(executor.submit(getLyrics, song))
 for i, future in enumerate(as_completed(futures)):
-    try:
-        lyrics += future.result()
-    except Exception as e:
-        print(f"song {i + 1}/{len(songs)} - {e}")
+    lyrics += future.result()
 
 with open("train.csv", "w") as file:
     csv = writer(file)
