cb3d44a104ec59aaf42593e8ef998cdf9395fd02
33tm <80350771+33tm@users.noreply.github.com>
2024-07-25 15:41:30 -0700

    restructure

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 from os import mkdir
-from re import match
 from csv import writer
+from json import loads
 from requests import get
+from re import match, sub
 from bs4 import BeautifulSoup
 from datetime import timedelta
 from timeit import default_timer
@@ -77,25 +78,49 @@ def getLyrics(url):
     except Exception as e:
         print(f"{e.__class__.__name__} - {url}")
         return []
+    
+if not exists("out"):
+    mkdir("out")
 
-if exists("temp/songs.txt"):
-    with open("temp/songs.txt", "r") as file:
+if exists("out/songs.txt"):
+    with open("out/songs.txt", "r") as file:
         songs = file.read().splitlines()
 else:
     with ThreadPoolExecutor(max_workers=100) as executor:
         futures = [executor.submit(getSongs, i) for i in range(1465)]
     songs = {song for future in as_completed(futures) for song in future.result()}
-    if not exists("temp"):
-        mkdir("temp")
     if len(songs):
-        with open("temp/songs.txt", "w") as file:
+        with open("out/songs.txt", "w") as file:
             file.write("\n".join(songs))
 
 with ThreadPoolExecutor(max_workers=100) as executor:
     futures = [executor.submit(getLyrics, song) for song in songs]
 words = {word for future in as_completed(futures) for word in future.result()}
 
-with open("train.csv", "w", encoding="utf-8", newline="") as file:
+if exists("out/dictionary.jsonl"):
+    with open("out/dictionary.jsonl", encoding="utf-8") as file:
+        dictionary = file.read().splitlines()
+else:
+    res = get("https://kaikki.org/dictionary/Korean/kaikki.org-dictionary-Korean.jsonl").text
+    dictionary = res.splitlines()
+    with open("out/dictionary.jsonl", "w", encoding="utf-8") as file:
+        file.write(res)
+
+for word in dictionary:
+    if not word:
+        continue
+    word = loads(word)
+    if "forms" not in word:
+        continue
+    for form in word["forms"]:
+        if "roman" not in form or "form" not in form:
+            continue
+        romaja = sub(r"[^a-z\s]+", "", form["roman"])
+        korean = sub(r"[^가-힣\s]", "", form["form"])
+        for r, k in zip(romaja.split(), korean.split()):
+            words.add((r, k))
+
+with open("out/data.csv", "w", encoding="utf-8", newline="") as file:
     writer(file).writerows(words)
 
 print(f"\nscraped {len(words)} pairs ({len(songs)} songs) in {getElapsed()}")
---
+++ model.py
@@ -0,0 +1,80 @@
+from csv import reader
+from os.path import exists
+from torch import torch, nn, optim
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if not exists("out/data.csv"):
+    print("Please generate the dataset with \"data.py\"")
+    exit()
+
+with open("out/data.csv", encoding="utf-8", newline="") as file:
+    data = [tuple(row) for row in reader(file)]
+    print(f"Loaded {len(data)} pairs")
+    romaja, korean = zip(*data)
+
+class Initialize:
+    def __init__(self, words):
+        self.words = words
+        self.charset = list(set("".join(words)))
+        self.max = max([len(word) for word in words])
+        self.tensors = []
+
+def decompose(word):
+    output = ""
+    for syllable in word:
+        if (u := ord(syllable) - 0xAC00) < 0:
+            raise Exception(f'Non-Korean syllable "{syllable}" in word "{word}"')
+        l = chr(u // 588 + 0x1100)
+        v = chr(u % 588 // 28 + 0x1161)
+        t = chr(u % 28 + 0x11A7) if u % 28 else ""
+        output += l + v + t
+    return output
+
+romaja, korean = Initialize(romaja), Initialize([decompose(word) for word in korean])
+
+charset_max = max(len(romaja.charset), len(korean.charset))
+
+class RNN(nn.Module):
+    def __init__(self, device):
+        super(RNN, self).__init__()
+        self.device = device
+        self.rnn = nn.RNN(charset_max, 128, batch_first=True).to(device)
+        self.linear = nn.Linear(128, charset_max).to(device)
+    
+    def forward(self, input):
+        hidden = torch.zeros(1, input.size(0), 128).to(self.device)
+        output, hidden = self.rnn(input, hidden)
+        return self.linear(output[:, -1, :])
+    
+model = RNN(device).to(device)
+    
+def create_tensors(input: Initialize):
+    for word in input.words:
+        tensor = torch.zeros(
+            max(romaja.max, korean.max),
+            1,
+            charset_max,
+        ).to(device)
+        for i, char in enumerate(word):
+            tensor[i][0][input.charset.index(char)] = 1
+        input.tensors.append(tensor)
+
+create_tensors(romaja)
+create_tensors(korean)
+    
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+for epoch in range(100):
+    total_loss = 0
+    for r, k in zip(romaja.tensors, korean.tensors):
+        r, k = r.to(device), k.to(device)
+        optimizer.zero_grad()
+        loss = criterion(model(r), k.squeeze())
+        total_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+    print(f'Epoch [{epoch + 1}/{100}], Loss: {(total_loss / len(romaja.tensors)):.4f}')
+
+torch.jit.script(model).save("out/model.pt")
