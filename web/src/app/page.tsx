7aeae6fb41ca7bfed544be8d33d28a0c483af507
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-07-30 20:46:34 -0700

    mobile optimization

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,27 +6,37 @@ import { FaGithub, FaInstagram, FaXTwitter } from "react-icons/fa6"
 export default () => {
     return (
         <>
-            <div className="pl-4 pt-4">
-                <h1 className="text-3xl font-bold">Aengmusae (앵무새)</h1>
-                <p>Convert romanized Korean text into Korean Hangul using AI</p>
+            <div className="absolute flex justify-between w-[calc(100vw-4rem)] md:w-auto top-12 left-12 md:top-auto md:bottom-12">
+                <Link href="https://tttm.us">
+                    <Logo className="h-12 md:h-24 fill-accent" />
+                </Link>
+                <div className="flex md:flex-col my-auto md:m-0 pr-8 md:pr-0 md:w-auto md:ml-8 md:justify-between space-x-4 md:space-x-0 md:space-y-2">
+                    <Link href="https://github.com/33tm">
+                        <FaGithub
+                            size={24}
+                            className="hover:opacity-80 transition-opacity duration-200"
+                        />
+                    </Link>
+                    <Link href="https://instagram.com/33tmmm">
+                        <FaInstagram
+                            size={24}
+                            className="hover:opacity-80 transition-opacity duration-200"
+                        />
+                    </Link>
+                    <Link href="https://x.com/33tmmm">
+                        <FaXTwitter
+                            size={24}
+                            className="hover:opacity-80 transition-opacity duration-200"
+                        />
+                    </Link>
+                </div>
             </div>
-            <Aengmusae />
-            <Link href="https://tttm.us">
-                <Logo className="absolute bottom-12 left-12 h-24 fill-accent" />
-            </Link>
-            <div className="absolute flex flex-col justify-between h-24 bottom-12 left-44 space-y-2">
-                <Link className="flex group" href="https://github.com/33tm">
-                    <FaGithub size={24} />
-                    <p className="pl-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">@33tm</p>
-                </Link>
-                <Link className="flex group" href="https://instagram.com/33tmmm">
-                    <FaInstagram size={24} />
-                    <p className="pl-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">@33tmmm</p>
-                </Link>
-                <Link className="flex group" href="https://x.com/33tmmm">
-                    <FaXTwitter size={24} />
-                    <p className="pl-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">@33tmmm</p>
-                </Link>
+            <div className="absolute flex flex-col h-[calc(100vh-2rem)] w-[calc(100vw-2rem)] align-middle text-center">
+                <div className="m-auto text-center mx-4">
+                    <h1 className="text-3xl font-bold">Aengmusae (앵무새)</h1>
+                    <p>Convert romanized Korean text into Korean Hangul using AI</p>
+                    <Aengmusae />
+                </div>
             </div>
             <div className="absolute bottom-12 right-12 text-right">
                 <p>
--- web/src/components/aengmusae.tsx
+++ web/src/components/aengmusae.tsx
@@ -20,36 +20,36 @@ export const Aengmusae = () => {
         }).then(async res => {
             if (!res.ok) setError(true)
             else setOutput(await res.text())
-        })
-    }
-
-    if (error) {
-        return (
-            <div className="flex">
-                <p className="m-auto text-2xl pt-8 font-semibold">
-                    Service unavailable, API is offline.
-                </p>
-            </div>
-        )
+        }).catch(() => setError(true))
     }
 
     return (
-        <>
-            <form onSubmit={submit}>
-                <input
-                    type="text"
-                    className="bg-background border-2 border-accent rounded-l-lg p-2 ml-4 mt-4 focus:outline-none"
-                    placeholder="annyeong"
-                    onChange={({ target }) => setInput(target.value)}
-                />
-                <button
-                    type="submit"
-                    className="p-2 pr-3 bg-accent border-2 border-accent rounded-r-lg text-background"
-                >
-                    Submit
-                </button>
-            </form>
-            <p className="p-4 text-4xl font-semibold">{output}</p>
-        </>
+        <div className="flex w-full mt-4">
+            {error ? (
+                <p className="m-auto text-xl font-semibold">
+                    Service unavailable, server offline.
+                </p>
+            ) : (
+                <>
+                    <form onSubmit={submit} className="flex flex-col md:flex-row m-auto">
+                        <input
+                            type="text"
+                            className="bg-background p-2 border-2 border-accent rounded-t-lg md:rounded-none md:rounded-l-lg focus:outline-none"
+                            placeholder="annyeong"
+                            onChange={({ target }) => setInput(target.value)}
+                        />
+                        <button
+                            type="submit"
+                            className="bg-accent p-2 md:pr-3 border-2 border-accent rounded-b-lg md:rounded-none md:rounded-r-lg text-background"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                    <p className="absolute w-[calc(100vw-4rem)] mt-28 text-4xl text-center font-semibold">
+                        {output}
+                    </p>
+                </>
+            )}
+        </div >
     )
 }
